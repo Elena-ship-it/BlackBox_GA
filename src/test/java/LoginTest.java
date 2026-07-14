3d54cfa476acfa90e16e805358e5ad5acea1f483
Elena Izmailova <Radosturadostu@gmail.com>
2026-07-14 13:37:46 +0300

    Merge remote-tracking branch 'origin/FT-6' into FT-6

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,30 +1,21 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class LoginTest {
     @Test
     public void checkLogin() {
-        //1. открыть браузер
-        //2. зайти https://www.saucedemo.com/
-        // 3. Нажать кнопку Login 
-        // 4. Закрыть браузер после теста
-
         WebDriver browser = new ChromeDriver();
-        browser.get("https://www.saucedemo.com/");
-
-        // Ввод логина
-        browser.findElement(By.xpath("//*[@placeholder='Username']")).sendKeys("standard_user");
-
-        // Ввод пароля
-        browser.findElement(By.xpath("//*[@placeholder='Password']")).sendKeys("secret_sauce");
-
-        // Нажать кнопку Login
-        browser.findElement(By.id("login-button")).click();
-
-        // Закрыть браузер после теста
-        browser.quit();
-
+        try {
+            browser.get("https://www.saucedemo.com/");
+            browser.findElement(By.id("user-name")).sendKeys("standard_user");
+            browser.findElement(By.id("password")).sendKeys("secret_sauce");
+            browser.findElement(By.id("login-button")).click();
+            Assert.assertTrue(browser.getCurrentUrl().contains("inventory"));
+        } finally {
+            browser.quit();
+        }
     }
 }
